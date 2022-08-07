2ddda67e7446320ba138bf8462bfb27489469a52
NaT5uK1 <o0desolation@hotmail.com>
2022-08-08 06:27:14 +0800

    fixed:icon required bug on windows building

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,10 +1,3 @@
-use tauri_build::{try_build, Attributes, WindowsAttributes};
 fn main() {
-    // tauri_build::build()
-    if let Err(error) = try_build(
-        Attributes::new()
-            .windows_attributes(WindowsAttributes::new().window_icon_path("res/icon.ico")),
-    ) {
-        panic!("Error: {}", error);
-    }
+  tauri_build::build()
 }
