aefc8a4ae7c376c6a2fba1ce91c441e603b91fd4
NaT5uK1 <o0desolation@hotmail.com>
2023-05-28 10:51:06 +0800

    fix:responsive style && .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { rules } from "./data/rules.json"
 function App() {
   return (
     <div className="h-screen bg-indigo-400 relative">
-      <div className="min-h-1/3 flex justify-center items-center space-x-30 sm:space-x-40 md:space-x-50 lg:space-x-60 xl:lg:space-x-70">
+      <div className="min-h-1/3 flex justify-center items-center space-x-20 sm:space-x-40 md:space-x-60 lg:space-x-80 xl:lg:space-x-100">
         {rules.map((rule, index) => (
           <CustomLink to={`calc/${index}`} key={index}>
             {rule.name}
